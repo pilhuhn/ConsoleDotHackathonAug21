e11fa9033b0c1ce5207fcfbf575e642ad8e0a3f2
Heiko W. Rupp <hwr@pilhuhn.de>
2021-08-16 16:15:54 +0200

    Switch from polling to interrupts

--- ConsoleDotHackathonAug21.ino
+++ ConsoleDotHackathonAug21.ino
@@ -2,29 +2,43 @@
    Arduino as external Keyboard.
    Needs an Arduino that is able to use the keyboard libraries.
    See https://www.arduino.cc/reference/en/language/functions/usb/keyboard/
-   
+
 */
 
 #include <Keyboard.h>
 
-#define STOP_PIN 9        // pull to gnd to end keyboard functionality
-#define BUTTON_PIN1 10
-#define BUTTON_PIN2 11
+#define STOP_PIN 0        // pull to gnd to end keyboard functionality
+#define BUTTON_PIN1 1
+#define BUTTON_PIN2 2
+
+// #define DEBUG   // when uncommented enables serial output instead of keyboard.
 
 boolean shouldBlink = true;
 
-void setup() {
-  
-  pinMode(13, OUTPUT);
-  pinMode(STOP_PIN, INPUT);
-  digitalWrite(STOP_PIN, HIGH);    // Pull up
+// Array to map pin to emitted character
+int pinToChar[] = { 'X', 'Y'} ;
 
-  pinMode(BUTTON_PIN1, INPUT);
-  digitalWrite(BUTTON_PIN1, HIGH); // Pull up
-  pinMode(BUTTON_PIN2, INPUT);
-  digitalWrite(BUTTON_PIN2, HIGH); // Pull up
-  
+volatile char val = -1;
+
+
+void setup() {
+
+  pinMode(13, OUTPUT);
+  for (int i = 0; i <= 2; i++) {
+    pinMode(i, INPUT);        // Pin is for input
+    digitalWrite(i, HIGH);    // Pull up. so it
+  }
+
+  // Start acting as a HID
+#ifdef DEBUG
+  Serial.begin(115200);
+#else
   Keyboard.begin();
+#endif  
+  // Attach the interrupt handlers
+  attachInterrupt(digitalPinToInterrupt(0), shouldEnd, FALLING);
+  attachInterrupt(digitalPinToInterrupt(1), sendKey, FALLING);
+  attachInterrupt(digitalPinToInterrupt(2), sendKey, FALLING);
 }
 
 void loop() {
@@ -36,16 +50,34 @@ void loop() {
     delay(100);
   }
 
-  if (digitalRead(BUTTON_PIN1) == 0) {  // if the button goes low
-    Keyboard.write('x');  // send a 'x' to the computer via Keyboard HID
-  }
-  if (digitalRead(BUTTON_PIN2) == 0) { // if the button goes low
-    Keyboard.write('X');  // send 'X' to the computer via Keyboard HID
-  }
-  if (digitalRead(STOP_PIN) == 0) { // if the button goes low
-    Keyboard.end();  // end sending stuff via Keyboard HID
-    shouldBlink = false;
+  // If the interrups routine set a value we emit it.
+  if (val != -1) {
+#ifdef DEBUG    
+    Serial.println(val);
+    Serial.flush();
+#else    
+    Keyboard.write(val);
+#endif    
+    val = -1;
   }
 
-  delay(100);  // delay so there aren't a ton of x's
+}
+
+void shouldEnd() {
+  if (digitalRead(STOP_PIN) == LOW) {
+#ifndef DEBUG    
+    Keyboard.end();  // end sending stuff via Keyboard HID
+#endif    
+    shouldBlink = false;
+  }
+}
+
+void sendKey() {
+  // figure out which pin was pressed
+  for (int i = 1; i <= 2 ; i++) {
+    if (digitalRead(i) == LOW) {
+      val = pinToChar[i - 1];
+      break;
+    }
+  }
 }
